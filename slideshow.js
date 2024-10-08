2e5ea1f450022742922b70d6f8742b0e747b6294
shlema <shlema@gmail.com>
2024-10-08 11:10:42 +0200

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -10,11 +10,11 @@ function showSlides() {
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+//  for (i = 0; i < dots.length; i++) {
+//    dots[i].className = dots[i].className.replace(" active", "");
+// }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  // dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 1000); // Change image every 1 second
 }
 
